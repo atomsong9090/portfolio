6312413a3c2a58e7f92a0be0b5bc87203a01fdd9
atomsong92 <66257825+atomsong92@users.noreply.github.com>
2020-07-09 22:04:28 +0900

    Add: js transparent_home

--- main.js
+++ main.js
@@ -29,6 +29,14 @@ const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
+
+//Make home slowely fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
